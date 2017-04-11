501e683b5f202e2c0520f0a7411bc7a0c8957c25
康永敢 <kangyonggan@gmail.com>
2017-04-11 18:19:43 +0800

    发布文章

--- dfjz-web/src/main/java/com/kangyonggan/app/dfjz/web/controller/ArticleController.java
+++ dfjz-web/src/main/java/com/kangyonggan/app/dfjz/web/controller/ArticleController.java
@@ -3,11 +3,13 @@ package com.kangyonggan.app.dfjz.web.controller;
 import com.kangyonggan.app.dfjz.biz.service.ArticleService;
 import com.kangyonggan.app.dfjz.biz.service.CommentService;
 import com.kangyonggan.app.dfjz.biz.service.RedisService;
+import com.kangyonggan.app.dfjz.biz.util.PropertiesUtil;
 import com.kangyonggan.app.dfjz.common.IPUtil;
 import com.kangyonggan.app.dfjz.common.MarkdownUtil;
 import com.kangyonggan.app.dfjz.model.dto.Toc;
 import com.kangyonggan.app.dfjz.model.vo.Article;
 import com.kangyonggan.app.dfjz.model.vo.Comment;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,6 +28,7 @@ import java.util.List;
  */
 @Controller
 @RequestMapping("article")
+@Log4j2
 public class ArticleController extends BaseController {
 
     @Autowired
@@ -92,5 +95,28 @@ public class ArticleController extends BaseController {
     public String comment(@ModelAttribute("comment") @Valid Comment comment, HttpServletRequest request) {
         articleService.updateArticleCommentCount(comment, IPUtil.getIp(request));
         return "redirect:/article/" + comment.getArticleId();
+    }
+
+    /**
+     * 保存文章
+     *
+     * @param article
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "save", method = RequestMethod.POST)
+    public String save(@ModelAttribute("article") @Valid Article article, HttpServletRequest request) {
+        String ip = IPUtil.getIp(request);
+        log.info("发布文章的ip实际地址为：{}", ip);
+        String ipWhite = PropertiesUtil.getProperties("article.publish.ip.white");
+        log.info("发布文章的ip白名单为：{}", ipWhite);
+
+        if (ipWhite.indexOf(ip) > -1) {
+            log.info("文章发布成功");
+        } else {
+            log.error("{}不在ip白名单中，企图发布文章，已报警！", ip);
+        }
+
+        return "redirect:/index";
     }
 }
