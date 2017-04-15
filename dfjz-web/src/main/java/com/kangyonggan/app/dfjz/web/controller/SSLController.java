b0d72539af363ce235ebbcdc8592815199ac4507
康永敢 <kangyonggan@gmail.com>
2017-04-15 14:05:36 +0800

    ssl

---
+++ dfjz-web/src/main/java/com/kangyonggan/app/dfjz/web/controller/SSLController.java
@@ -0,0 +1,24 @@
+package com.kangyonggan.app.dfjz.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author kangyonggan
+ * @since 2017/4/15 0015
+ */
+@RestController
+@RequestMapping(".well-known/acme-challenge")
+public class SSLController {
+
+    @RequestMapping("y1WhC3cdHAFNWeNwigLlWxVBVLJdWTW7R4a591IqnEA")
+    public String one() {
+        return "y1WhC3cdHAFNWeNwigLlWxVBVLJdWTW7R4a591IqnEA.sFeQZnTv-wjzr0FAwLceuz2VOy-wwIvsczHrRIPwOzU";
+    }
+
+    @RequestMapping("WcH7OZsgtg-chfkEzsEYAA12brKsG5pCv2DShVdqA9I")
+    public String two() {
+        return "WcH7OZsgtg-chfkEzsEYAA12brKsG5pCv2DShVdqA9I.sFeQZnTv-wjzr0FAwLceuz2VOy-wwIvsczHrRIPwOzU";
+    }
+
+}
