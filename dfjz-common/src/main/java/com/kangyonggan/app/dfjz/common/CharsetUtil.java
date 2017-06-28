8009d6b4e4fc611d56e2809f97289141d3eb7dc5
kangyonggan <kyg@kangyonggans-MacBook-Pro.local>
2017-06-28 13:47:12 +0800

    转码工具

--- dfjz-common/src/main/java/com/kangyonggan/app/dfjz/common/CharsetUtil.java
+++ dfjz-common/src/main/java/com/kangyonggan/app/dfjz/common/CharsetUtil.java
@@ -25,16 +25,16 @@ public class CharsetUtil {
     }
 
     public static String uf8ToGbk(String data) {
-        try {
-            return new String(data.getBytes("UTF-8"), "GBK");
-        } catch (Exception e) {
-            return data;
-        }
+        return convertCharset(data, "UTF-8", "GBK");
     }
 
     public static String gbkToUtf8(String data) {
+        return convertCharset(data, "GBK", "UTF-8");
+    }
+
+    public static String convertCharset(String data, String oldCharset, String newOldCharset) {
         try {
-            return new String(data.getBytes("GBK"), "UTF-8");
+            return new String(data.getBytes(oldCharset), newOldCharset);
         } catch (Exception e) {
             return data;
         }
